b99bc8b56ad0e871d7a83e38ed26083419c4ffce
Pierre-Louis Nordmann <pierrelouis.nordmann@gmail.com>
2026-09-01 00:48:00 +0200

    test: added a test for basic failure modes

---
+++ testbench/failures/heartbeat_runtime_error.vhd
@@ -0,0 +1,31 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity heartbeat is
+  port ( clk: out std_logic);
+end heartbeat;
+
+architecture behaviour of heartbeat
+is
+  constant clk_period : time := 10 ns;
+begin
+  -- Clock process definition
+  clk_process: process
+  begin
+    clk <= '0';
+    wait for clk_period/2;
+    clk <= '1';
+    wait for clk_period/2;
+  end process;
+
+  -- Crashing process definition
+  crash_proc : process
+  begin
+      wait for 100 ns;
+      assert false
+          report "Intentional runtime failure"
+          severity failure;
+      wait;
+  end process;
+
+end behaviour;
---
+++ testbench/failures/test_failures.py
@@ -0,0 +1,49 @@
+from cocotb.handle import HierarchyObject
+from cocotb.triggers import Timer
+
+from cocotest import DUTSpec
+
+# Regular heartbeat, no RTL error here.
+dut_heartbeat = DUTSpec(
+    "ghdl",
+    ["testbench/heartbeat.vhd"],
+    "heartbeat",
+    "vhdl",
+    ["--std=08"],
+    ["--std=08"],
+)
+
+
+async def test_python_error(dut_heartbeat: HierarchyObject):
+    await Timer(1, unit="us")
+    assert False, "this is a python error"
+
+
+# RTL file with a syntax error.
+dut_syntax_error = DUTSpec(
+    "ghdl",
+    ["testbench/failures/heartbeat_syntax_error.vhd"],
+    "heartbeat",
+    "vhdl",
+    ["--std=08"],
+    ["--std=08"],
+)
+
+
+async def test_build_error(dut_syntax_error: HierarchyObject):
+    await Timer(1, unit="us")
+
+
+# RTL file with a runtime error.
+dut_runtime_error = DUTSpec(
+    "ghdl",
+    ["testbench/failures/heartbeat_runtime_error.vhd"],
+    "heartbeat",
+    "vhdl",
+    ["--std=08"],
+    ["--std=08"],
+)
+
+
+async def test_runtime_error(dut_runtime_error: HierarchyObject):
+    await Timer(1, unit="us")
